bcbed2cfc66cc5a612d909932b5efef619426c2f
Stilyan Kangalov <stilyan.kangalov@gmail.com>
2021-08-08 14:42:25 +0300

    Added - Read more - button

--- src/components/VehicleList/coverImage.js
+++ src/components/VehicleList/coverImage.js
@@ -2,10 +2,13 @@ import React from "react";
 
 let MainImage = ({ media, name }) => {
     return (
-        <picture>
-            <source media="(min-width: 1000px)" srcSet={media[0].url} />
-            <img src={media[1].url} alt={name} style={{ width: "auto" }} />
-        </picture>
+        <div className="image-container">
+            <picture>
+                <source media="(min-width: 1000px)" srcSet={media[0].url} />
+                <img src={media[1].url} alt={name} style={{ width: "auto" }} />
+            </picture>
+            <div className="centered-text">Read more</div>
+        </div>
     );
 };
 
